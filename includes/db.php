586970a459cff38bfe51ddefec4b6ad7f6c1c357
Teong Zhong Ming <93188004+Teong-Zhong-Ming@users.noreply.github.com>
2025-05-08 15:04:59 +0800

    test db connection ok

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,15 @@
 <?php
 $serverName = "localhost";
-$connectionInfo = array("Database" => "YourDB", "UID" => "your_user", "PWD" => "your_password");
+$connectionInfo = array("Database" => "Healthcare_Database", 
+                        "UID" => "test1", 
+                        "PWD" => "password123");
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 if (!$conn) {
+    echo "Connection failed:<br>";
     die(print_r(sqlsrv_errors(), true));
+} else {
+    // Connection successful
+    // You can now execute queries or perform other database operations
+    echo "Connected to the database successfully.";
 }
 ?>
--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,7 @@
 <h1>Welcome to the Medical App</h1>
 <p>This is the homepage.</p>
+
+<?php 
+    include __DIR__ . '/../includes/db.php';
+
+?>
