a8101b3dc7a34df57b9a7c4d09db4e62b8dcad61
DylanChengZX <97202336+DylanChengZX@users.noreply.github.com>
2025-05-14 23:39:10 +0800

    frontend V3

--- pages/adminPage.php
+++ pages/adminPage.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-    /*session_start();
+    session_start();
     if (!isset($_SESSION['user_id'], $_SESSION['role']) || $_SESSION['role'] !== 'Admin') {
         header("Location: ../index.php?page=home");
         exit;
@@ -10,7 +10,7 @@
     echo "<h1>Welcome, " . htmlspecialchars($_SESSION['username']) . "</h1>";
     echo "<p>Your role is: " . htmlspecialchars($_SESSION['role']) . "</p>";
     echo "<p>Your ID is: " . htmlspecialchars($_SESSION['user_id']) . "</p>";
-    */
+    
 ?>
 <?php 
 
